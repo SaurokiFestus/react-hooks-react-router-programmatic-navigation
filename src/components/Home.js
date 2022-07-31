9311cb36d3b7d1b0df292efbb848a635b7ab75a8
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-07-31 11:41:20 +0300

    commit tree 117555bb6a44018e2a9ecd6c70d2f401b18b203c

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 function Home({ isLoggedIn }) {
+  //if the user is not logged in, take them to back to the login page
   if (!isLoggedIn) return <Redirect to="/login" />;
 
   return (
